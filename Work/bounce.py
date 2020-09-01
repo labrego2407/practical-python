b431adefcba42c8457a2325eb8a76669eaac2c4c
Larz <luis.abrego@gmail.com>
2020-09-01 17:32:21 -0500

    Bounce exercise
    First exercise

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,17 @@
 # bounce.py
 #
 # Exercise 1.5
+# 3/5 = 3 divided by 5 = 0.6
+# 100/0.6 = 60 or 3/5 the first height the ball fell
+
+max_bounce_height = 0.6
+current_height = 100
+bounce_height = 0
+bounces = 0
+
+while current_height > 1:
+    print(bounces, round(current_height * max_bounce_height, 4))
+    current_height = current_height * max_bounce_height
+    bounces += 1
+
+print('Number of bounces: ', bounces) 
